4397c56753de5ebad655e67c8b73737c9f8833a9
ChidoW <chidow@centridsol.tech>
2020-10-06 10:39:31 +0200

    Updates to the tree theme

--- node-content-renderer.js
+++ node-content-renderer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import styles from './node-content-renderer.scss';
+import styles from './node-content-renderer-centrid.scss'
 
 function isDescendant(older, younger) {
   return (
